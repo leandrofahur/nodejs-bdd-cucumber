220b8c8c593614019b0e596aa033c3b2e6b46325
Leandro <fahurleandro@gmail.com>
2021-03-31 18:54:04 -0700

    downloading file using multer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// const formidable = require("formidable");
+// const StringDecoder = require("string_decoder").StringDecoder;
+const fs = require("fs");
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const express = require("express");
 const app = express();
 
@@ -22,6 +28,31 @@ app.post("/", (req, res) => {
   index === -1 ? data.push({ name, age }) : data;
 
   res.status(200).json({ method: "POST", statusCode: 200, message: data });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const tmpPath = req.file.path;
+  const targetPath = "uploads/" + req.file.originalname;
+  const src = fs.createReadStream(tmpPath);
+  const dst = fs.createWriteStream(targetPath);
+
+  src.pipe(dst);
+  src.on("end", () => {
+    res.status(200).json({
+      method: "POST",
+      statusCode: 200,
+      route: "/upload",
+      message: "Upload Works",
+    });
+  });
+  src.on("error", () => {
+    res.status(500).json({
+      method: "POST",
+      statusCode: 500,
+      route: "/upload",
+      message: "Upload failed",
+    });
+  });
 });
 
 const PORT = process.env.PORT || 7000;
